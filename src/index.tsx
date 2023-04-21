e43883c91b7c4401cf4b103af42e20932587c6fa
Yuriy <105667257+yuriystepaniuk@users.noreply.github.com>
2023-04-21 12:29:29 +0300

    add auth0

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@material-tailwind/react";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +13,15 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <Auth0Provider
+        domain="dev-66xe7bmsdc8hzolp.us.auth0.com"
+        clientId="ZksQgZEfWYtsY5HJPjAIJc4JNEL9bQ4l"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
